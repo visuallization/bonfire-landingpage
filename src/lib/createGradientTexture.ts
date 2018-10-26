181877df3ca833940851bb6c98eef1ea6099ec78
Florentin Rieger <florentin.rieger@gmail.com>
2018-10-26 19:56:13 +0200

    chore(remove glow and vignette texture and create it on the fly)

--- src/lib/createGradientTexture.ts
+++ src/lib/createGradientTexture.ts
@@ -1,13 +1,23 @@
-export default function createGradientTexture(width: number, height: number, colors: string[]) {
+import { GRADIENT } from '../constants';
+
+export default function createGradientTexture(width: number, height: number, colors: string[], type?: GRADIENT, r1: number = 2, r2:number = 3) {
   const canvas = document.createElement('canvas');    
   canvas.width  = width;
   canvas.height = height;
   const ctx: any = canvas.getContext('2d');
-  const gradient = ctx.createLinearGradient(0, 0, 0, height);
+
+  let gradient = ctx.createLinearGradient(0, 0, 0, height);
+
+  if (type === GRADIENT.RADIAL) {
+    gradient = ctx.createRadialGradient(width / 2, height / 2, width / r1, width / 2, height / 2, width / r2);
+  }
+
   colors.forEach((color, i) => {
     gradient.addColorStop( i / (colors.length - 1), color);
   });
+
   ctx.fillStyle = gradient;    
   ctx.fillRect(0, 0, width, height);
+  
   return PIXI.Texture.fromCanvas(canvas);
 }
