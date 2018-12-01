4862ea053ec2cc6a57e81f96c60587eee17f9ce6
Florentin Rieger <florentin.rieger@gmail.com>
2018-12-01 14:03:34 +0100

    chore(clean up): console.logs and replace favicon

--- src/lambda/subscribe.js
+++ src/lambda/subscribe.js
@@ -5,11 +5,8 @@ const { MAILCHIMP_API_KEY, MAILING_LIST_ID } = process.env;
 const mailchimp = new MailChimp(MAILCHIMP_API_KEY);
 
 export function handler(event, context, callback) {
-  console.log("EVENT: ", event.body);
-
   return mailchimp.post(`/lists/${MAILING_LIST_ID}/members`, { email_address: event.body, status: 'subscribed' })
   .then(function(results) {
-    console.log('RESULTS:', results);
     return {
       statusCode: 200,
       body: JSON.stringify(results)
